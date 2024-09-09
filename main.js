31e09317de4a9636c478ea90e0344d1b18ade956
Jesse-Driskill <jesse@driskill.com>
2024-09-09 03:57:47 -0700

    Makes slideshow animation more fluid, changes font-sizes

--- main.js
+++ main.js
@@ -10,6 +10,114 @@ document.addEventListener("DOMContentLoaded", () => {
     let imageContainerCount;
     let overlay = document.getElementById("overlay1");
     let xOutOverlayIcon = document.getElementById("x-out-overlay-icon");
+    let leftArrowClickActive = true;
+    let rightArrowClickActive = true;
+
+    let imagesObj = {
+        1: {
+            name: "Dramatic Cliffscape",
+            price: "$120/month",
+            gallons: "20 Gallon"
+        },
+        2: {
+            name: "Fast-Moving Jungle Stream",
+            price: "$200/month",
+            gallons: "50 Gallon"
+        },
+        3: {
+            name: "The Trio Terrascapes",
+            price: "$75/month",
+            gallons: "variable"
+        },
+        4: {
+            name: "Driftwood Peaks",
+            price: "$200/month",
+            gallons: "50 Gallon"
+        },
+        7: {
+            name: "Jungle Orb",
+            price: "$50/month",
+            gallons: "5 Gallon"
+        },
+        8: {
+            name: "Convex Mountain",
+            price: "$50/month",
+            gallons: "5 Gallon"
+        },
+        6: {
+            name: "Tree of Life",
+            price: "$150/month",
+            gallons: "30 Gallon"
+        },
+        5: {
+            name: "Mountain Paludarium",
+            price: "$50/month",
+            gallons: "7.5 Gallon"
+        },
+        9: {
+            name: "TBD",
+            price: "TBD",
+            gallons: "TBD"
+        },
+    };
+
+    function generateImageSubtext(imgNum) {
+        let nameDiv = document.createElement("div");
+        let priceDiv = document.createElement("div");
+        let gallonDiv = document.createElement("div");
+
+        nameDiv.classList.add("small-font");
+        priceDiv.classList.add("small-font");
+        gallonDiv.classList.add("small-font");
+
+        nameDiv.innerHTML = imagesObj[imgNum]["name"];
+        priceDiv.innerHTML = imagesObj[imgNum]["price"];
+        gallonDiv.innerHTML = imagesObj[imgNum]["gallons"];
+
+        let imageTextContainer = document.createElement("div");
+        imageTextContainer.appendChild(nameDiv);
+        imageTextContainer.appendChild(priceDiv);
+        imageTextContainer.appendChild(gallonDiv);
+        imageTextContainer.classList.add("image-text-container");
+
+        return imageTextContainer;
+    }
+
+    function generateImage(imgNum) {
+        let img = document.createElement("img");
+        img.src = `assets/FishTank${imgNum}.jpg`
+
+        img.classList.add("slideshow-image");
+        img.id = `FishTank${imgNum}`;
+
+        img.addEventListener("click", () => {
+            overlay.style.opacity = 1;
+            overlay.style.position = "fixed";
+            
+            let overlayImg = document.createElement("img");
+            overlayImg.src = img.src;
+            overlayImg.style.maxWidth = "90%";
+            overlayImg.style.maxHeight = "90%";
+            overlayImg.id = "overlay-img";
+            overlayImg.classList.add("invisible");
+            overlay.appendChild(overlayImg);
+            xOutOverlayIcon.style.display = "block";
+            
+            setTimeout(() => {
+                overlayImg.style.opacity = 1;
+            }, 1);
+        });
+
+        let imgDiv = document.createElement("div");
+        imgDiv.classList.add("single-image-container");
+
+        imgDiv.appendChild(img);
+        // imgDiv.appendChild(generateImageSubtext(imgNum));
+        
+        
+    
+        return imgDiv;
+    }
 
     xOutOverlayIcon.addEventListener("click", () => {
         overlay.style.opacity = 0;
@@ -28,38 +136,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 imageContainer.classList.add("slideshow-group");
                 imageContainer.id = `slideshow-group-${i + 1}`;
                 if (i !== 0) {
-                    // imageContainer.classList.add("hidden");
                     imageContainer.classList.add("hidden");
                 }
                 for (let k = 0; k < 3; k++) {
                     if (i * 3 + k >= imageCount) {
                         break;
                     }
-    
-                    let img = document.createElement("img");
-                    img.src = `assets/FishTank${(i * 3) + k + 1}.jpg`
-                    // img.style.height = "10vw";
-                    img.classList.add("slideshow-image");
-    
-                    img.addEventListener("click", () => {
-                        overlay.style.opacity = 1;
-                        overlay.style.position = "fixed";
-                        
-                        let overlayImg = document.createElement("img");
-                        overlayImg.src = img.src;
-                        overlayImg.style.maxWidth = "90%";
-                        overlayImg.style.maxHeight = "90%";
-                        overlayImg.id = "overlay-img";
-                        overlayImg.classList.add("invisible");
-                        overlay.appendChild(overlayImg);
-                        xOutOverlayIcon.style.display = "block";
-                        
-                        setTimeout(() => {
-                            overlayImg.style.opacity = 1;
-                        }, 1)
-                    });
-    
-                    imageContainer.appendChild(img);
+
+                    imageContainer.appendChild(generateImage((i*3)+k+1))
     
                 }
             }
@@ -70,35 +154,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 slideshowDiv.appendChild(imageContainer);
                 imageContainer.classList.add("slideshow-group");
                 imageContainer.id = `slideshow-group-${i + 1}`;
-                // imageContainer.width = "75%"
                 if (i !== 0) {
-                    // imageContainer.classList.add("hidden");
                     imageContainer.classList.add("hidden");
                 }
     
-                let img = document.createElement("img");
-                img.src = `assets/FishTank${i + 1}.jpg`;
-                // img.style.maxWidth = "60hw";
-                img.classList.add("slideshow-image");
-                img.addEventListener("click", () => {
-                    overlay.style.opacity = 1;
-                    overlay.style.position = "fixed";
-                    
-                    let overlayImg = document.createElement("img");
-                    overlayImg.src = img.src;
-                    overlayImg.style.maxWidth = "90%";
-                    overlayImg.style.maxHeight = "90%";
-                    overlayImg.id = "overlay-img";
-                    overlayImg.classList.add("invisible");
-                    overlay.appendChild(overlayImg);
-                    xOutOverlayIcon.style.display = "block";
-                    
-                    setTimeout(() => {
-                        overlayImg.style.opacity = 1;
-                    }, 1)
-                });
+                
 
-                imageContainer.appendChild(img);
+                imageContainer.appendChild(generateImage(i+1));
             }
         }
     }
@@ -108,40 +170,88 @@ document.addEventListener("DOMContentLoaded", () => {
     let slideshowImages = slideshowDiv.children;
 
     let pointer = 0;
-    let slideshowLength = slideshowImages.length;
     let slideshowLeftArrow = document.getElementById("slideshow-left-arrow");
     let slideshowRightArrow = document.getElementById("slideshow-right-arrow");
 
 
     slideshowLeftArrow.addEventListener("click", () => {
-        slideshowImages[pointer].classList.remove("shown-flex");
-        slideshowImages[pointer].classList.add("hidden");
-        slideshowImages[pointer].classList.remove("slide-effect");
-        if (pointer === 0) {
-            pointer = slideshowImages.length - 1;
-        } else {
-            pointer -= 1;
+        if (leftArrowClickActive) {
+            leftArrowClickActive = false;
+            
+            slideshowImages[pointer].classList.add("slide-out-to-right-effect"); //instantly have current images slide out to left on click
+            slideshowImages[pointer].style.position = "absolute";//change to absolute to prevent page interaction during animation
+            slideshowImages[pointer].style.left = "0";
+            let prev;
+
+            if (pointer === 0) {
+                pointer = slideshowImages.length - 1;
+                prev = 0;
+            } else {
+                pointer -= 1;
+                prev = pointer + 1;
+            }
+
+            slideshowImages[pointer].classList.remove("hidden");
+            slideshowImages[pointer].classList.add("shown-flex");
+            slideshowImages[pointer].classList.remove("slide-in-from-left-effect");
+            slideshowImages[pointer].classList.add("slide-in-from-left-effect");
+            
+
+            setTimeout(() => {
+                slideshowImages[prev].classList.remove("shown-flex");
+                slideshowImages[prev].classList.add("hidden");
+                slideshowImages[prev].classList.remove("slide-in-from-left-effect");
+                slideshowImages[prev].classList.remove("slide-out-to-right-effect");
+                slideshowImages[prev].style.position = "relative";
+                slideshowImages[pointer].classList.remove("slide-in-from-left-effect");
+            }, 645);
+            
+            setTimeout(() => {
+                leftArrowClickActive = true;
+            }, 1000);
+            
         }
-        slideshowImages[pointer].classList.remove("hidden");
-        slideshowImages[pointer].classList.add("shown-flex");
-        slideshowImages[pointer].classList.toggle("slide-effect");
+        
+
     });
 
     slideshowRightArrow.addEventListener("click", () => {
+        if (rightArrowClickActive) {
+            rightArrowClickActive = false;
+            
+            slideshowImages[pointer].classList.add("slide-out-to-left-effect"); //instantly have current images slide out to left on click
+            slideshowImages[pointer].style.position = "absolute";//change to absolute to prevent page interaction during animation
+            slideshowImages[pointer].style.left = "0";
+            let prev;
 
-        slideshowImages[pointer].classList.add("hidden");
-        slideshowImages[pointer].classList.remove("shown-flex");
-        slideshowImages[pointer].classList.remove("slide-effect");
+            if (pointer === slideshowImages.length - 1) {
+                pointer = 0
+                prev = slideshowImages.length - 1;
+            } else {
+                pointer += 1;
+                prev = pointer - 1;
+            }
 
+            slideshowImages[pointer].classList.remove("hidden");
+            slideshowImages[pointer].classList.add("shown-flex");
+            slideshowImages[pointer].classList.remove("slide-in-from-right-effect");
+            slideshowImages[pointer].classList.add("slide-in-from-right-effect");
+            
 
-        if (pointer === slideshowImages.length - 1) {
-            pointer = 0;
-        } else {
-            pointer += 1;
+            setTimeout(() => {
+                slideshowImages[prev].classList.remove("shown-flex");
+                slideshowImages[prev].classList.add("hidden");
+                slideshowImages[prev].classList.remove("slide-in-from-right-effect");
+                slideshowImages[prev].classList.remove("slide-out-to-left-effect");
+                slideshowImages[prev].style.position = "relative";
+                slideshowImages[pointer].classList.remove("slide-in-from-right-effect");
+            }, 645);
+            
+            setTimeout(() => {
+                rightArrowClickActive = true;
+            }, 1000);
         }
-        slideshowImages[pointer].classList.remove("hidden");
-        slideshowImages[pointer].classList.add("shown-flex");
-        slideshowImages[pointer].classList.toggle("slide-effect");
+
     });
 
     let getQuoteNavLink = document.getElementById("get-quote-nav-link");
@@ -170,12 +280,6 @@ document.addEventListener("DOMContentLoaded", () => {
             }, 100)
     
         } else {
-            // quotePage.classList.add("hidden");
-            // quotePage.classList.remove("shown-block");
-            // homePage.classList.remove("hidden");
-            // homePage.classList.add("shown-block");
-            // currentPage = "home";
-
             let count = 0;
 
             quotePage.style.opacity = 0;
@@ -255,7 +359,6 @@ document.addEventListener("DOMContentLoaded", () => {
     
 
     document.getElementById("logo").addEventListener("mouseover", () => {
-        // document.getElementById("audio1").volume = 100;
         document.getElementById("audio1").play();
     });
 
@@ -284,6 +387,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-
-    // slideshowDiv.appendChild(slideshowImg1);
 });
